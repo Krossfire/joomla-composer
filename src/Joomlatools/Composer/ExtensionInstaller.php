bd1579de91147f67f90c91c6ff7de8a47aea7210
Dariusz Sadkowski <darek@w3a.pl>
2015-03-25 23:14:49 +0100

    Playing with functions

--- src/Joomlatools/Composer/ExtensionInstaller.php
+++ src/Joomlatools/Composer/ExtensionInstaller.php
@@ -2,9 +2,9 @@
 /**
  * Joomlatools Composer plugin - https://github.com/joomlatools/joomla-composer
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://github.com/joomlatools/joomla-composer for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://github.com/joomlatools/joomla-composer for the canonical source repository
  */
 
 namespace Joomlatools\Composer;
@@ -23,264 +23,272 @@ use Composer\Installer\LibraryInstaller;
  */
 class ExtensionInstaller extends LibraryInstaller
 {
-    protected $_config      = null;
-    protected $_application = null;
-    protected $_credentials = array();
+	protected $_config = null;
+	protected $_application = null;
+	protected $_credentials = array();
 
-    /**
-     * {@inheritDoc}
-     */
-    public function __construct(IOInterface $io, Composer $composer, $type = 'library')
-    {
-        parent::__construct($io, $composer, $type);
+	/**
+	 * {@inheritDoc}
+	 */
+	public function __construct(IOInterface $io, Composer $composer, $type = 'library')
+	{
+		parent::__construct($io, $composer, $type);
 
-        $this->_config = $composer->getConfig();
+		$this->_config = $composer->getConfig();
 
-        $this->_initialize();
-    }
+		$this->_initialize();
+	}
 
-    /**
-     * Initializes extension installer.
-     *
-     * @return void
-     */
-    protected function _initialize()
-    {
-        $config = $this->_config->get('joomla');
+	/**
+	 * Initializes extension installer.
+	 *
+	 * @return void
+	 */
+	protected function _initialize()
+	{
+		$config = $this->_config->get('joomla');
 
-        if(is_null($config) || !is_array($config)) {
-            $config = array();
-        }
+		if (is_null($config) || !is_array($config))
+		{
+			$config = array();
+		}
 
-        $defaults = array('name'      => 'root',
-            'username'  => 'root',
-            'groups'    => array(8),
-            'email'     => 'root@localhost.home');
+		$defaults = array('name'     => 'root',
+		                  'username' => 'root',
+		                  'groups'   => array(8),
+		                  'email'    => 'root@localhost.home');
 
-        $this->_credentials = array_merge($defaults, $config);
+		$this->_credentials = array_merge($defaults, $config);
 
-        $this->_bootstrap();
-    }
+		$this->_bootstrap();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getInstallPath(PackageInterface $package)
-    {
-        return 'tmp/' . $package->getPrettyName();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getInstallPath(PackageInterface $package)
+	{
+		return 'tmp/' . $package->getPrettyName();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        parent::install($repo, $package);
+	/**
+	 * {@inheritDoc}
+	 */
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		parent::install($repo, $package);
 
-        $this->_setupExtmanSupport($package);
+		$this->_setupExtmanSupport($package);
 
-        $this->io->write('    <fg=cyan>Installing</fg=cyan> into Joomla'.PHP_EOL);
+		$this->io->write('    <fg=cyan>Installing</fg=cyan> into Joomla' . PHP_EOL);
 
-        if(!$this->_application->install($this->getInstallPath($package)))
-        {
-            // Get all error messages that were stored in the message queue
-            $descriptions = $this->_getApplicationMessages();
+		if (!$this->_application->install($this->getInstallPath($package)))
+		{
+			// Get all error messages that were stored in the message queue
+			$descriptions = $this->_getApplicationMessages();
 
-            $error = 'Error while installing '.$package->getPrettyName();
-            if(count($descriptions)) {
-                $error .= ':'.PHP_EOL.implode(PHP_EOL, $descriptions);
-            }
+			$error = 'Error while installing ' . $package->getPrettyName();
+			if (count($descriptions))
+			{
+				$error .= ':' . PHP_EOL . implode(PHP_EOL, $descriptions);
+			}
 
-            throw new \RuntimeException($error);
-        }
-    }
+			throw new \RuntimeException($error);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
-    {
-        parent::update($repo, $initial, $target);
+	/**
+	 * {@inheritDoc}
+	 */
+	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+	{
+		parent::update($repo, $initial, $target);
 
-        $this->_setupExtmanSupport($target);
+		$this->_setupExtmanSupport($target);
 
-        $this->io->write('    <fg=cyan>Updating</fg=cyan> Joomla extension'.PHP_EOL);
+		$this->io->write('    <fg=cyan>Updating</fg=cyan> Joomla extension' . PHP_EOL);
 
-        if(!$this->_application->update($this->getInstallPath($target)))
-        {
-            // Get all error messages that were stored in the message queue
-            $descriptions = $this->_getApplicationMessages();
+		if (!$this->_application->update($this->getInstallPath($target)))
+		{
+			// Get all error messages that were stored in the message queue
+			$descriptions = $this->_getApplicationMessages();
 
-            $error = 'Error while updating '.$target->getPrettyName();
-            if(count($descriptions)) {
-                $error .= ':'.PHP_EOL.implode(PHP_EOL, $descriptions);
-            }
+			$error = 'Error while updating ' . $target->getPrettyName();
+			if (count($descriptions))
+			{
+				$error .= ':' . PHP_EOL . implode(PHP_EOL, $descriptions);
+			}
 
-            throw new \RuntimeException($error);
-        }
-    }
+			throw new \RuntimeException($error);
+		}
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function supports($packageType)
-    {
-        return $packageType === 'joomlatools-installer';
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public function supports($packageType)
+	{
+		return $packageType === 'joomlatools-installer';
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        $installer = $this->_application->getInstaller();
-        $installer->setPath('source', $this->getInstallPath($package));
+	/**
+	 * {@inheritDoc}
+	 */
+	public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		$installer = $this->_application->getInstaller();
+		$installer->setPath('source', $this->getInstallPath($package));
 
-        $manifest = $installer->getManifest();
+		$manifest = $installer->getManifest();
 
-        if($manifest)
-        {
-            $type    = (string) $manifest->attributes()->type;
-            $element = $this->_getElementFromManifest($manifest);
+		if ($manifest)
+		{
+			$type    = (string) $manifest->attributes()->type;
+			$element = $this->_getElementFromManifest($manifest);
 
-            return !empty($element) ? $this->_application->hasExtension($element, $type) : false;
-        }
+			return !empty($element) ? $this->_application->hasExtension($element, $type) : false;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * Bootstraps the Joomla application
-     *
-     * @return void
-     */
-    protected function _bootstrap()
-    {
-        if(!defined('_JEXEC'))
-        {
-            $_SERVER['HTTP_HOST']   = 'localhost';
-            $_SERVER['HTTP_USER_AGENT'] = 'Composer';
+	/**
+	 * Bootstraps the Joomla application
+	 *
+	 * @return void
+	 */
+	protected function _bootstrap()
+	{
+		if (!defined('_JEXEC'))
+		{
+			$_SERVER['HTTP_HOST']       = 'localhost';
+			$_SERVER['HTTP_USER_AGENT'] = 'Composer';
 
-            define('_JEXEC', 1);
-            define('DS', DIRECTORY_SEPARATOR);
+			define('_JEXEC', 1);
+			define('DS', DIRECTORY_SEPARATOR);
 
-            define('JPATH_BASE', realpath('.'));
-            require_once JPATH_BASE . '/includes/defines.php';
+			define('JPATH_BASE', realpath('.'));
+			require_once JPATH_BASE . '/includes/defines.php';
 
-            require_once JPATH_BASE . '/includes/framework.php';
-            require_once JPATH_LIBRARIES . '/import.php';
+			require_once JPATH_BASE . '/includes/framework.php';
+			require_once JPATH_LIBRARIES . '/import.php';
 
-            require_once JPATH_LIBRARIES . '/cms.php';
-        }
+			require_once JPATH_LIBRARIES . '/cms.php';
+		}
 
-        if(!($this->_application instanceof Application))
-        {
-            $options = array('root_user' => $this->_credentials['username']);
+		if (!($this->_application instanceof Application))
+		{
+			$options = array('root_user' => $this->_credentials['username']);
 
-            $this->_application = new Application($options);
-            $this->_application->authenticate($this->_credentials);
-        }
-    }
+			$this->_application = new Application($options);
+			$this->_application->authenticate($this->_credentials);
+		}
+	}
 
-    /**
-     * Fetches the enqueued flash messages from the Joomla application object.
-     *
-     * @return array
-     */
-    protected function _getApplicationMessages()
-    {
-        $messages       = $this->_application->getMessageQueue();
-        $descriptions   = array();
+	/**
+	 * Fetches the enqueued flash messages from the Joomla application object.
+	 *
+	 * @return array
+	 */
+	protected function _getApplicationMessages()
+	{
+		$messages     = $this->_application->getMessageQueue();
+		$descriptions = array();
 
-        foreach($messages as $message)
-        {
-            if($message['type'] == 'error') {
-                $descriptions[] = $message['message'];
-            }
-        }
+		foreach ($messages as $message)
+		{
+			if ($message['type'] == 'error')
+			{
+				$descriptions[] = $message['message'];
+			}
+		}
 
-        return $descriptions;
-    }
+		return $descriptions;
+	}
 
-    protected function _setupExtmanSupport(PackageInterface $target)
-    {
-        // If we are installing a Joomlatools extension, make sure to load the ComExtmanDatabaseRowExtension class
-        $name = strtolower($target->getPrettyName());
-        $parts = explode('/', $name);
-        if($parts[0] == 'joomlatools' && $parts[1] != 'extman')
-        {
-            \JPluginHelper::importPlugin('system', 'koowa');
+	protected function _setupExtmanSupport(PackageInterface $target)
+	{
+		// If we are installing a Joomlatools extension, make sure to load the ComExtmanDatabaseRowExtension class
+		$name  = strtolower($target->getPrettyName());
+		$parts = explode('/', $name);
+		if ($parts[0] == 'joomlatools' && $parts[1] != 'extman')
+		{
+			\JPluginHelper::importPlugin('system', 'koowa');
 
-            if(class_exists('Koowa') && !class_exists('ComExtmanDatabaseRowExtension')) {
-                \KObjectManager::getInstance()->getObject('com://admin/extman.database.row.extension');
-            }
-        }
-    }
+			if (class_exists('Koowa') && !class_exists('ComExtmanDatabaseRowExtension'))
+			{
+				\KObjectManager::getInstance()->getObject('com://admin/extman.database.row.extension');
+			}
+		}
+	}
 
-    protected function _getElementFromManifest($manifest)
-    {
-        $element    = '';
-        $type       = (string) $manifest->attributes()->type;
-        $prefix     = isset($this->_prefixes[$type]) ? $this->_prefixes[$type].'_' : 'com_';
+	protected function _getElementFromManifest($manifest)
+	{
+		$element = '';
+		$type    = (string) $manifest->attributes()->type;
+		$prefix  = isset($this->_prefixes[$type]) ? $this->_prefixes[$type] . '_' : 'com_';
 
-        switch($type)
-        {
-            case 'module':
-                if(count($manifest->files->children()))
-                {
-                    foreach($manifest->files->children() as $file)
-                    {
-                        if((string) $file->attributes()->module)
-                        {
-                            $element = (string) $file->attributes()->module;
-                            break;
-                        }
-                    }
-                }
-                break;
-            case 'plugin':
-                if(count($manifest->files->children()))
-                {
-                    foreach($manifest->files->children() as $file)
-                    {
-                        if ((string) $file->attributes()->$type)
-                        {
-                            $element = (string) $file->attributes()->$type;
-                            break;
-                        }
-                    }
-                }
-                break;
-            case 'template':
-		$element = strtolower((string) $manifest->name);
-                $element = preg_replace('/[^A-Z0-9_\.-]/i', '', $element);
-		break;
-            case 'component':
-            default:
-                $element = strtolower((string) $manifest->name);
-                $element = preg_replace('/[^A-Z0-9_\.-]/i', '', $element);
+		switch ($type)
+		{
+			case 'module':
+				if (count($manifest->files->children()))
+				{
+					foreach ($manifest->files->children() as $file)
+					{
+						if ((string) $file->attributes()->module)
+						{
+							$element = (string) $file->attributes()->module;
+							break;
+						}
+					}
+				}
+				break;
+			case 'plugin':
+				if (count($manifest->files->children()))
+				{
+					foreach ($manifest->files->children() as $file)
+					{
+						if ((string) $file->attributes()->$type)
+						{
+							$element = (string) $file->attributes()->$type;
+							break;
+						}
+					}
+				}
+				break;
+			case 'template':
+				$element = strtolower((string) $manifest->name);
+				$element = preg_replace('/[^A-Z0-9_\.-]/i', '', $element);
+				break;
+			case 'component':
+			default:
+				$element = strtolower((string) $manifest->name);
+				$element = preg_replace('/[^A-Z0-9_\.-]/i', '', $element);
 
-                if(substr($element, 0, 4) != 'com_') {
-                    $element = 'com_'.$element;
-                }
-                break;
-        }
+				if (substr($element, 0, 4) != 'com_')
+				{
+					$element = 'com_' . $element;
+				}
+				break;
+		}
 
-        return $element;
-    }
+		return $element;
+	}
 
-    public function __destruct()
-    {
-        if(!defined('_JEXEC')) {
-            return;
-        }
+	public function __destruct()
+	{
+		if (!defined('_JEXEC'))
+		{
+			return;
+		}
 
-        // Clean-up to prevent PHP calling the session object's __destruct() method;
-        // which will burp out Fatal Errors all over the place because the MySQLI connection
-        // has already closed at that point.
-        $session = \JFactory::$session;
-        if(!is_null($session) && is_a($session, 'JSession')) {
-            $session->close();
-        }
-    }
+		// Clean-up to prevent PHP calling the session object's __destruct() method;
+		// which will burp out Fatal Errors all over the place because the MySQLI connection
+		// has already closed at that point.
+		$session = \JFactory::$session;
+		if (!is_null($session) && is_a($session, 'JSession'))
+		{
+			$session->close();
+		}
+	}
 }
